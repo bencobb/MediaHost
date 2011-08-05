9e83314635ce14738a28f0dd325482abcc290448
Ben Cobb <ben@swiftsetup.com>
2011-08-05 14:40:01 -0600

    Updated SQL

--- SQL/MediaHostSchema.sql
+++ SQL/MediaHostSchema.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `mediahost` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `mediahost`;
--- MySQL dump 10.13  Distrib 5.1.40, for Win32 (ia32)
+-- MySQL dump 10.13  Distrib 5.5.9, for Win32 (x86)
 --
 -- Host: localhost    Database: mediahost
 -- ------------------------------------------------------
--- Server version	5.5.13
+-- Server version	5.5.15
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +16,14 @@ USE `mediahost`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `mediahost`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `mediahost` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `mediahost`;
+
+--
 -- Table structure for table `entity`
 --
 
@@ -28,8 +34,61 @@ CREATE TABLE `entity` (
   `Id` bigint(20) NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `group`
+--
+
+DROP TABLE IF EXISTS `group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `group` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `EntityId` bigint(20) DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `EntityId` (`EntityId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `mediafile`
+--
+
+DROP TABLE IF EXISTS `mediafile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mediafile` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `EntityId` bigint(20) DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `RelativeFilePath` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `EntityId` (`EntityId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `playlist`
+--
+
+DROP TABLE IF EXISTS `playlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `playlist` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `EntityId` bigint(20) DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `EntityId` (`EntityId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'mediahost'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -40,4 +99,4 @@ CREATE TABLE `entity` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-08-04 13:23:14
+-- Dump completed on 2011-08-05 14:37:12
